fac0d78b109c24def21e1cb1a4ddb430e7bbc97e
Guillaume ICHTER <ichter.g@gmail.com>
2022-02-01 13:22:21 +0100

    model creation

--- softdesk/models.py
+++ softdesk/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(max_length=1500, blank=True, default='')
+    type = models.CharField(max_length=100, blank=True)
+    author_user_id = models.ForeignKey(
+        User, related_name='projects', on_delete=models.CASCADE)
+
+
+class Issues(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(max_length=1500, blank=True, default='')
+    tag = models.CharField(max_length=100, blank=True, default='')
+    priority = models.CharField(max_length=100, blank=True, default='')
+    status = models.CharField(max_length=100, blank=True, default='')
+
+    author_user_id = models.ForeignKey(
+        User, related_name='issues_created', on_delete=models.CASCADE)
+    assignee_user_id = models.ForeignKey(
+        User, related_name='issues_assigned', on_delete=models.SET_NULL, null=True)
+    project_id = models.ForeignKey(
+        Projects, related_name='issues', on_delete=models.CASCADE)
+
+
+class Comments(models.Model):
+    description = models.TextField(max_length=1500, blank=True, default='')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    author_user_id = models.ForeignKey(
+        User, related_name='comments_created', on_delete=models.CASCADE)
+    issue_id = models.ForeignKey(
+        Issues, related_name='comments', on_delete=models.CASCADE)
