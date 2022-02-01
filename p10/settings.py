3e50f031db6524009564d06a1b8e59450b975f39
Guillaume ICHTER <ichter.g@gmail.com>
2022-02-01 13:22:45 +0100

    migration

--- p10/settings.py
+++ p10/settings.py
@@ -138,3 +138,7 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
